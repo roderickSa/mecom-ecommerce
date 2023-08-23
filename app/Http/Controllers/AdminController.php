d0a4298e54b39e3a7d76c9d02faae7e47de30d27
roderickSanchez <roderick.sanchez@justo.mx>
2023-08-23 12:50:14 -0500

    ADMIN PAGES: dasboard + profile + change password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view("admin.admin_dashboard");
+        return view("admin.index");
+    }
+
+    public function AdminLogin()
+    {
+        return view("admin.admin_login");
+    }
+
+    public function AdminProfile()
+    {
+        $adminData = User::findOrFail(auth()->user()->id);
+        return view("admin.admin_profile", compact("adminData"));
+    }
+
+    public function AdminChangePassword()
+    {
+        $adminData = User::findOrFail(auth()->user()->id);
+        return view("admin.admin_change_password", compact("adminData"));
+    }
+
+    public function AdminProfileStore(Request $request)
+    {
+        $adminData = User::findOrFail(auth()->user()->id);
+        $adminData->name = $request->name;
+        $adminData->phone = $request->phone;
+        $adminData->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path("upload/admin_images/" . $adminData->photo));
+            $filename = date("YmdHis") . $file->getClientOriginalName();
+            $file->move(public_path("upload/admin_images"), $filename);
+            $adminData["photo"] = $filename;
+        }
+        $adminData->save();
+
+        $notification = ['message' => 'Admin Profile has been sucessfully updated', 'alert-type' => "success"];
+
+        return redirect()->back()->with($notification);
+    }
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => ['required', "confirmed"],
+        ]);
+
+        if (!Hash::check($request->old_password, auth()->user()->password)) {
+            return back()->with("error", "Old password doesn't match!!");
+        }
+
+        User::where('id', auth()->user()->id)->update(["password" => bcrypt($request->new_password)]);
+
+        return back()->with("status", "Password Changed Successfully");
+    }
+
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,11 +33,18 @@ Route::middleware('auth')->group(function () {
 ///Admin Dashboard
 Route::middleware(["auth", "role:admin"])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name("admin.dashboard");
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name("admin.logout");
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name("admin.profile");
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name("admin.profile.store");
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name("admin.change.password");
+    Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])->name("admin.update.password");
 });
 
 ///Vendor Dashboard
 Route::middleware(["auth", "role:vendor"])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name("vendor.dashboard");
 });
+
+Route::get("/admin/login", [AdminController::class, "AdminLogin"])->name("admin.login");
 
 require __DIR__ . '/auth.php';
